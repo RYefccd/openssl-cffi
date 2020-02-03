135e56cb28946431eef3789b8a8657045dab269a
RYefccd <847960106@qq.com>
2020-02-03 16:32:22 +0800

    python cffi call gmssl

--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 openssl-cffi
 ============
 
+
+    # system libraries
+    apt-get install build-essential python3-dev libffi-dev
+    # python libraries
+    python3 -m pip install cffi
+
+
+download gmssl(openssl fork, surpport sm2, sm3, sm4), and compile it
+docs: http://gmssl.org/docs/quickstart.html
+
+after install gmssl,
+
+    python3 openssl-cffi.py
+
+
+上面的实现了 python 通过调用 gmssl 中的 aes 算法. 之后会使用此方法调用gmssl 中的 sm2, sm4.
+
+
+
+下面的链接是 openssl 1.0.x 的实现, 有些接口发生了变化.
+gmssl 是基于 openssl 1.1.x 的 fork 版本, 所以有些EVP的接口发生了变化, 以我的实际代码为准， 这个说明我得抽时间改下.
+调用细节以我的实际代码openssl-cffi.py 为准，原理不变, 可以参考这个文档.
+
 This is example code for wrapping openssl with cffi. More information in the blog post:
 
 [Using Openssl From Python With `python-cffi`](https://gist.github.com/vishvananda/980132c0970f8621bb3c)
--- openssl-cffi.py
+++ openssl-cffi.py
@@ -13,17 +13,23 @@ def _encrypt_decrypt_ecb_shell(data, key, decrypt):
             '-nopad',
             '-in', infile,
             '-out', outfile,
-            '-K', key.encode("hex")]
+            '-K', key.encode().hex()] # key.encode("hex")
+    #print(" ".join(cmd))
     subprocess.Popen(cmd).wait()
-    with open(outfile) as f:
+    with open(outfile, "rb") as f:
         result = f.read()
     return result
 
 def encrypt_ecb_shell(data, key):
-    return _encrypt_decrypt_ecb_shell(data, key, False)
+    if type(data) == str:
+       data = data.encode()
+    return _encrypt_decrypt_ecb_shell(data, key, False)  # data is bytes
 
 def decrypt_ecb_shell(data, key):
-    return _encrypt_decrypt_ecb_shell(data, key, True)
+    if type(data) == str:
+       data = data.encode()
+    b =_encrypt_decrypt_ecb_shell(data, key, True)   # data is bytes
+    return b.decode()
 
 import cffi
 
@@ -35,6 +41,9 @@ int encrypt_ecb(unsigned char * input, unsigned char * output,
 int decrypt_ecb(unsigned char * input, unsigned char * output,
                 unsigned char * key, int len);
 """)
+
+# set_source
+# verify
 _C = _FFI.verify("""
 #include <openssl/evp.h>
 
@@ -42,6 +51,7 @@ int encrypt_ecb(unsigned char * input, unsigned char * output,
                 unsigned char * key, int len)
 {
   int outlen, finallen;
+/*
   EVP_CIPHER_CTX ctx;
   EVP_CIPHER_CTX_init(&ctx);
   EVP_EncryptInit(&ctx, EVP_aes_128_ecb(), key, 0);
@@ -49,6 +59,15 @@ int encrypt_ecb(unsigned char * input, unsigned char * output,
   if(!EVP_EncryptUpdate(&ctx, output, &outlen, input, len)) return 0;
   if(!EVP_EncryptFinal(&ctx, output + outlen, &finallen)) return 0;
   EVP_CIPHER_CTX_cleanup(&ctx);
+*/
+  EVP_CIPHER_CTX *ctx;
+  ctx=EVP_CIPHER_CTX_new();
+  EVP_CIPHER_CTX_init(ctx);
+  EVP_EncryptInit(ctx, EVP_aes_128_ecb(), key, 0);
+  EVP_CIPHER_CTX_set_padding(ctx, 0);
+  if(!EVP_EncryptUpdate(ctx, output, &outlen, input, len)) return 0;
+  if(!EVP_EncryptFinal(ctx, output + outlen, &finallen)) return 0;
+  EVP_CIPHER_CTX_cleanup(ctx);
   return outlen + finallen;
 }
 
@@ -56,6 +75,7 @@ int decrypt_ecb(unsigned char * input, unsigned char * output,
                 unsigned char * key, int len)
 {
   int outlen, finallen;
+/*
   EVP_CIPHER_CTX ctx;
   EVP_CIPHER_CTX_init(&ctx);
   EVP_DecryptInit(&ctx, EVP_aes_128_ecb(), key, 0);
@@ -63,52 +83,75 @@ int decrypt_ecb(unsigned char * input, unsigned char * output,
   if(!EVP_DecryptUpdate(&ctx, output, &outlen, input, len)) return 0;
   if(!EVP_DecryptFinal(&ctx, output + outlen, &finallen)) return 0;
   EVP_CIPHER_CTX_cleanup(&ctx);
+*/
+  EVP_CIPHER_CTX *ctx;
+  ctx=EVP_CIPHER_CTX_new();
+  EVP_CIPHER_CTX_init(ctx);
+  EVP_DecryptInit(ctx, EVP_aes_128_ecb(), key, 0);
+  EVP_CIPHER_CTX_set_padding(ctx, 0);
+  if(!EVP_DecryptUpdate(ctx, output, &outlen, input, len)) return 0;
+  if(!EVP_DecryptFinal(ctx, output + outlen, &finallen)) return 0;
+  EVP_CIPHER_CTX_cleanup(ctx);
+
   return outlen + finallen;
 }
-""", libraries=["crypto"], extra_compile_args=['-Wno-deprecated-declarations'])
+""", 
+include_dirs=["/usr/local/include/"],
+libraries=["crypto"], 
+library_dirs=["/usr/local/lib/"], 
+extra_compile_args=['-Wno-deprecated-declarations'])
 
 def encrypt_ecb_cffi(data, key):
+    data = data.encode()
+    key_bytes = key.encode()
     datalen = len(data)
     out = _FFI.new("char[%s]" % (datalen))
-    num = _C.encrypt_ecb(data, out, key, datalen)
-    return _FFI.string(out, num)
+    num = _C.encrypt_ecb(data, out, key_bytes, datalen)
+    ffi_bytes = _FFI.string(out, num)
+    #print("ffi_ecb_encrypt:", ffi_bytes)
+    return ffi_bytes
 
 def decrypt_ecb_cffi(data, key):
+    #print("decrypt data:", data)
+    key_bytes = key.encode()
     datalen = len(data)
     out = _FFI.new("char[%s]" % (datalen))
-    num = _C.decrypt_ecb(data, out, key, datalen)
-    return _FFI.string(out, num)
+    num = _C.decrypt_ecb(data, out, key_bytes, datalen)
+    ffi_bytes = _FFI.string(out, num)
+    ffi_str = ffi_bytes.decode()
+    #print("ffi_ecb_decrypt:", ffi_str)
+    return ffi_str
 
 data = "Some data that has multiple sixteen byte blocks."
 key =  "needstobesixteen"
 
-print "Verifying encryption..."
+print("Verifying encryption...")
 assert(decrypt_ecb_shell(encrypt_ecb_shell(data, key), key) == data)
 assert(decrypt_ecb_cffi(encrypt_ecb_cffi(data, key), key) == data)
 assert(decrypt_ecb_cffi(encrypt_ecb_shell(data, key), key) == data)
 assert(decrypt_ecb_shell(encrypt_ecb_cffi(data, key), key) == data)
-print "VERIFIED"
+print("VERIFIED")
 
 import time
 
-print "Profiling shell version..."
+print("Profiling shell version...")
 
 start = time.time()
 
-for i in xrange(100):
+for i in range(100):
     decrypt_ecb_shell(encrypt_ecb_shell(data, key), key)
 
 shell = time.time() - start
-print "SHELL:", shell
+print("SHELL:", shell)
 
-print "Profiling cffi version..."
+print("Profiling cffi version...")
 start = time.time()
 
-for i in xrange(100):
+for i in range(100):
     decrypt_ecb_cffi(encrypt_ecb_cffi(data, key), key)
 
 cffi = time.time() - start
-print "CFFI:", cffi
+print("CFFI:", cffi)
 
-print
-print "Using cffi is roughly %dx faster than shelling out." % (shell / cffi)
+print()
+print("Using cffi is roughly %dx faster than shelling out." % (shell / cffi))
